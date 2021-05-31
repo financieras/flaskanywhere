e05e0cf7a26e771cb03d7fd1b464b4006575406b
addolfo <financieras@gmail.com>
2021-05-31 17:11:39 +0200

    pru2

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def webhook():
 
 @app.route('/')
 def hello():
-    return '<h1>hello there from PythonAnywhere</h1><p>Continuous Deployment of a Python Flask Application.</p>'
+    return '<p>Hello there from PythonAnywhere</p><p>Continuous Deployment of a Python Flask Application.</p>'
 if __name__ == '__main__':
     app.run(debug=True)
