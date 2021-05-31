da48d4d1de6db8a807722efde3e52d45d9ecbdf8
addolfo <financieras@gmail.com>
2021-05-31 12:59:28 +0200

    print

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
+    print("Estoy aqui.")
     if request.method == 'POST':
         repo = git.Repo('.')
         origin = repo.remotes.origin
