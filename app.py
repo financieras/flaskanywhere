3b9aec92319c6ee6110ffbdc17987ff61fd78b51
addolfo <financieras@gmail.com>
2021-06-01 20:02:04 +0200

    Nueva ruta

--- app.py
+++ app.py
@@ -2,13 +2,24 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
 from flask.wrappers import Response
+import git
 
 app = Flask(__name__)
 
+#Con este codigo el webhook de GitHub no da error
 @app.route('/update_server', methods=['POST'])
 def update_server():
     print(request.json)
     return Response(status=200)
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./flaskanywhere')
+    origin = repo.remotes.origin
+    repo.create_head('main', 
+    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/')
 def hello():
